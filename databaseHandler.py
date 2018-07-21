c0a7d082eddbb8ca8f86f6861dd11f0ee70bb709
czfloyd <floyd.c23@gmail.com>
2018-07-21 14:55:16 -0500

    Added more event creation database handling methods

--- databaseHandler.py
+++ databaseHandler.py
@@ -8,7 +8,7 @@ def initializeDatabase():
     c = conn.cursor()
 
     c.execute('''CREATE TABLE events
-    (key INTEGER PRIMARY KEY, title TEXT, date TEXT, cap INTEGER, creator TEXT, status INTEGER, details TEXT)''')
+    (key INTEGER PRIMARY KEY, title TEXT, date TEXT, cap INTEGER, visibility INTEGER, creator TEXT, creatorName TEXT, status INTEGER, details TEXT)''')
 
     c.execute('''CREATE TABLE attendee
     (key INTEGER PRIMARY KEY, name TEXT, phone TEXT, eventID INTEGER, status TEXT)''')
@@ -33,8 +33,63 @@ def nameEvent(creator, name):
     c = conn.cursor()
 
     try:
-        c.execute('UPDATE events SET name = {n} AND status = {ns} WHERE creator = {c} AND status = {os}'
+        c.execute('UPDATE events SET title = {n} AND status = {ns} WHERE creator = {c} AND status = {os}'
             .format(n = name, c = creator, os = EventState.EVENT_CREATED, ns = EventState.NAME_CREATED))
+        return True
+    except:
+        return False
+
+def setDatetimeEvent(creator, datetime):
+    conn = sqlite3.connect(fileName)
+    c = conn.cursor()
+
+    try:
+        c.execute('UPDATE events SET date = {d} AND status = {ns} WHERE creator = {c} AND status = {os}'
+            .format(d = datetime, c = creator, os = EventState.NAME_CREATED, ns = EventState.TIME_CREATED))
+        return True
+    except:
+        return False
+
+def setDescriptionEvent(creator, description):
+    conn = sqlite3.connect(fileName)
+    c = conn.cursor()
+
+    try:
+        c.execute('UPDATE events SET description = {d} AND status = {ns} WHERE creator = {c} AND status = {os}'
+            .format(d = description, c = creator, os = EventState.TIME_CREATED, ns = EventState.DESCRIPTION_CREATED))
+        return True
+    except:
+        return False
+
+def setCapEvent(creator, cap):
+    conn = sqlite3.connect(fileName)
+    c = conn.cursor()
+
+    try:
+        c.execute('UPDATE events SET cap = {p} AND status = {ns} WHERE creator = {c} AND status = {os}'
+            .format(p = cap, c = creator, os = EventState.DESCRIPTION_CREATED, ns = EventState.CAPACITY_CREATED))
+        return True
+    except:
+        return False
+
+def setVisibilityEvent(creator, visibility):
+    conn = sqlite3.connect(fileName)
+    c = conn.cursor()
+
+    try:
+        c.execute('UPDATE events SET visibility = {a} AND status = {ns} WHERE creator = {c} AND status = {os}'
+            .format(a = visibility, c = creator, os = EventState.CAPACITY_CREATED, ns = EventState.VISIBILITY_CREATED))
+        return True
+    except:
+        return False
+
+def setCreatorNameEvent(creator, name):
+    conn = sqlite3.connect(fileName)
+    c = conn.cursor()
+
+    try:
+        c.execute('UPDATE events SET creatorName = {n} AND status = {ns} WHERE creator = {c} AND status = {os}'
+            .format(n = name, c = creator, os = EventState.VISIBILITY_CREATED, ns = EventState.ORGANIZER_NAME_CREATED))
         return True
     except:
         return False
@@ -50,4 +105,3 @@ def hadUnfinishedEvent(creator):
         if createdEvent and createdEvent != EventState.EVENT_DONE:
             return True
     return False
-
--- webserver.py
+++ webserver.py
@@ -3,10 +3,7 @@ from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 from databaseHandler import *
 import os.path
-import datetime
-import vobject
-import requests
-from dateutil import parser
+
 # Events table
 #  table
 
@@ -19,46 +16,10 @@ def main():
         return Response('', 204)
 
 def handle_request(request_data):
-    body = request_data['Body']
-    phone_number = request_data['From']
-
-    if body == 'START':
-        if createEvent(phone_number):
-            response = MessagingResponse()
-            response.message("What would you like to name your event?")
-            return str(response)
-        else:
-            response = MessagingResponse()
-            response.message("Please finish editing your other event first")
-            return str(response)
-
-    elif nameEvent(body, phone_number):
-        response = MessagingResponse()
-        response.message('Your event has been called "{}"'.format(body))
-        return str(response)
-
-    elif addEventTime(parser.parse(body), phone_number):
-        response = MessagingResponse()
-        response.message('Your event has been scudled for {}'.format(body))
-        return str(response)
-
-
-def handle_vcf_url(url):
-    r = requests.get(url)
-    vcard = vobject.readOne(r.text)
-    tel = only_numerics(vcard.tel)
-    if(len(tel) == 10):
-        return "+1" + str(tel)
-    elif(len(tel) == 11):
-        return "+" + str(tel)
-    else:
-        raise TypeError('Cannot parse telephone number')
-
-def only_numerics(seq):
-    seq_type= type(seq)
-    return seq_type().join(filter(seq_type.isdigit, seq))
+    for key, value in request_data.items():
+        print(key, value)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
     if not os.path.isfile('database.db'):
         initializeDatabase()
+    app.run(host='0.0.0.0', port=5000)
