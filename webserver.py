04732cca5e07185ab6e5355e49136a5eaae95526
Grant Gollier <gragollier@gmail.com>
2018-07-21 12:12:02 -0500

    Begining database work

--- webserver.py
+++ webserver.py
@@ -1,21 +1,36 @@
 from flask import Flask, request, Response
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
 import sqlite3
+import os.path
 
 # Events table
-# User table
+#  table
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        handle_response(request.form)
+        handle_requst(request.form)
         return Response('', 204)
 
 def handle_requst(request_data):
     for key, value in request_data.items():
         print(key, value)
 
+def initializeDatabase():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE events
+    (key INTEGER PRIMARY KEY, title TEXT, date TEXT, cap INTEGER, creator TEXT, details TEXT)''')
+
+    c.execute('''CREATE TABLE attendee
+    (key INTEGER PRIMARY KEY, name TEXT, phone TEXT, eventID INTEGER, status TEXT)''')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=5000)
+    if not os.path.isfile('database.db'):
+        initializeDatabase()
