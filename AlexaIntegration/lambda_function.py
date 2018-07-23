ea2f3f89df18ba40f9b463e89451640ca318bc97
czfloyd <floyd.c23@gmail.com>
2018-07-22 23:12:41 -0500

    Interpretted the Alexa intents and performed sms sending

--- AlexaIntegration/lambda_function.py
+++ AlexaIntegration/lambda_function.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import json
+import requests
 from twilio.rest import Client
 
 account_sid = "AC81d02543e6fda0c44a977753c167e811"
@@ -30,9 +31,9 @@ def build_response(session_attributes, speechlet_response):
 def get_welcome_response():
 	#session_attributes = {}
 	card_title = "Welcome"
-	speech_output = "Hello World "
+	speech_output = "Welcome to Events Everywhere. Say 'start' to begin creating an event."
 	reprompt_text = None
-	should_end_session = True
+	should_end_session = False
 	return build_response({}, build_speechlet_response(speech_output, should_end_session))
 
 
@@ -48,10 +49,68 @@ def test_text(intent, session):
     #number = intent['slots']['number']['value']
     reprompt_text = None
     #name = returnName(number)
-    send_text()
+    #send_text("test", session_attributes.phone_number)
     speech_output = 'Text has been sent'
     should_end_session = True
     return build_response(session_attributes, build_speechlet_response(speech_output, should_end_session))
+
+
+def start_create(intent, session):
+    session_attributes = {}
+    speech_output = "To start, please tell me your phone number."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(speech_output, should_end_session))
+
+
+def phone_number(intent, session):
+    number = intent['slots']['phoneNumber']['value']
+    session['attributes']
+    session['attributes']['phone_number'] = number
+    reprompt_text = "What is your phone number?"
+    send_text("START", number)
+    speech_output = get_response_speech(1)
+    should_end_session = False
+    return build_response(session['attributes'], build_speechlet_response(speech_output, should_end_session))
+
+
+def  set_name(intent, session):
+    session['attributes']['event_name'] = intent['slots']['eventName']['value']
+    send_text(intent['slots']['eventName']['value'], session['attributes']['phone_number'])
+    speech_output = get_response_speech(2)
+    should_end_session = False
+    return build_response(session['attributes'], build_speechlet_response(speech_output, should_end_session))
+
+
+def  set_date(intent, session):
+    send_text(intent['slots']['date']['value'] + intent['slots']['time']['value'], session['attributes']['phone_number'])
+    send_text(session['attributes']['event_name'], session['attributes']['phone_number'])
+    speech_output = get_response_speech(3)
+    should_end_session = False
+    return build_response(session['attributes'], build_speechlet_response(speech_output, should_end_session))
+
+def  set_cap(intent, session):
+    send_text(intent['slots']['eventCapacity']['value'], session['attributes']['phone_number'])
+    speech_output = get_response_speech(4)
+    should_end_session = False
+    return build_response(session['attributes'], build_speechlet_response(speech_output, should_end_session))
+
+
+def  set_my_name(intent, session):
+    send_text(intent['slots']['myName']['value'], session['attributes']['phone_number'])
+    speech_output = get_response_speech(5)
+    should_end_session = False
+    return build_response(session['attributes'], build_speechlet_response(speech_output, should_end_session))
+
+
+def  invite(intent, session):
+    if (intent['slots']['inviteePhoneNumber']['value']):
+        send_text(intent['slots']['inviteePhoneNumber']['value'], session['attributes']['phone_number'])
+        speech_output = get_response_speech(6)
+    else:
+        send_text("DONE", session['attributes']['phone_number'])
+        speech_output = get_response_speech(7)
+    should_end_session = False
+    return build_response(session['attributes'], build_speechlet_response(speech_output, should_end_session))
 
 
 # --------------- Specific Events ------------------
@@ -62,6 +121,18 @@ def on_intent(intent_request, session):
 	intent_name = intent_request['intent']['name']
 	if intent_name == "TestText":
 		return test_text(intent, session)
+	elif intent_name == "StartCreate":
+		return start_create(intent, session)
+	elif intent_name == "SetPhoneNumber":
+		return phone_number(intent, session)
+	elif intent_name == "SetDate":
+		return set_date(intent, session)
+	elif intent_name == "SetCap":
+		return set_cap(intent, session)
+	elif intent_name == "SetMyName":
+		return set_my_name(intent, session)
+	elif intent_name == "Invite":
+		return invite(intent, session)
 	elif intent_name == "AMAZON.HelpIntent":
 		return get_welcome_response()
 	elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
@@ -98,10 +169,29 @@ def lambda_handler(event, context):
 
 # --------------- Data Handler ------------------
 
-def send_text():
+def send_text(text_body, phone_number):
     client = Client(account_sid, auth_token)
-    message = client.messages.create(
-                              body='Hello there!',
-                              from_='+12028038368',
+    '''message = client.messages.create(
+                              body= text_body,
+                              from_= phone_number,
                               to='+19133279103'
                           )
+    
+    print(message.sid)'''
+    r = requests.post("http://18.218.109.81", data={'Body': text_body, 'From': phone_number, 'NumMedia': 0})
+
+def get_response_speech(num):
+    if (num == 1):
+        return "Phone number received. What would you like to name your event?"
+    if (num == 2):
+        return "Event named. What date and time will the event be?"
+    if (num == 3):
+        return "Date set. How many people do you need?"
+    if (num == 4):
+        return "Event capacity set. What is your name?"
+    if (num == 5):
+        return "If you'd like to invite somebody, please say invite and then their phone number. Otherwise say done."
+    if (num == 6):
+        return "You can invite another person by again saying invite and then their phone number. Otherwise you can say done."
+    if (num == 7):
+        return "Thank you for creating an event with Events Everywhere. The people you invited have been notified. Goodbye."
